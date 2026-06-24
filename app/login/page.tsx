ae13a6c45f14221f17a392d9286a30cf334b0a77
Deepanshu Rana <ankurrana7830@gmail.com>
2026-06-24 21:58:43 +0530

    Fixed login page path and popup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { supabase } from "../utils/supabase";
+import { supabase } from "../../utils/supabase";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
